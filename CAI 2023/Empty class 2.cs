7c63a9ff9af7c7ffe6dc2ff91a564bdb0d8fce98
jprociuk <juanprociuk@gmail.com>
2023-09-07 19:36:08 -0300

    push 2

---
+++ CAI 2023/Empty class 2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CAI_2023
+{
+	public class Empty_class_2
+	{
+		public Empty_class_2()
+		{
+		}
+	}
+}
+
---
+++ CAI 2023/EmptyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CAI_2023
+{
+	public class EmptyClass
+	{
+		public EmptyClass()
+		{
+		}
+	}
+}
+
